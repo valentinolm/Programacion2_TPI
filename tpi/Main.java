de46b39ba51a4312f1b798f8702fedba9a5a7dfc
tTobo <tobiaspagniez1@gmail.com>
2025-06-23 22:15:01 -0300

    Update Main.java

--- tpi/Main.java
+++ tpi/Main.java
@@ -1,1 +1,9 @@
+package com.mycompany.tpi;
 
+import com.mycompany.tpi.controlador.ControladorTorneo;
+
+public class Main {
+    public static void main(String[] args) {
+        new ControladorTorneo().iniciar();
+    }
+}
