8edf98ce908cef32e0aa05dc8ec270a51ed86dbb
nazareno11 <nazarenoluna86@gmail.com>
2025-06-23 22:29:03 -0300

    Update ConexionBD.java

--- tpi/controlador/ConexionBD.java
+++ tpi/controlador/ConexionBD.java
@@ -8,7 +8,7 @@ import java.sql.SQLException;
 public class ConexionBD {
     private static final String URL = "jdbc:mysql://localhost:3306/torneo_champions";
     private static final String USER = "root";
-    private static final String PASSWORD = "123654"; 
+    private static final String PASSWORD = "contrasena"; 
 
     public static Connection conectar() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
