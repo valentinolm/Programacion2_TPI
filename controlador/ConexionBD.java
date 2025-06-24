9f1fc35dfb56a65aceb7804d5daef09bf4944c5a
Valentino Lozano <vlmvalenlozanomassey@gmail.com>
2025-06-23 22:10:09 -0300

    Update ConexionBD.java

--- controlador/ConexionBD.java
+++ controlador/ConexionBD.java
@@ -4,7 +4,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-//clase exclusivamente para conectar el programa a la base de datos
+//Creamos una clase exclusiva para conectar el programa a la base de datos
 public class ConexionBD {
     private static final String URL = "jdbc:mysql://localhost:3306/torneo_champions";
     private static final String USER = "root";
